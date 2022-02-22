0803bca29b8a4eb22e8aa91485b8aaeed89a7c7d
masoud-taheri <masoud.taheri.2008@gmail.com>
2022-02-22 22:48:08 +0330

    test

--- NumberGuessing/src/guessinggame/GuessingGame.java
+++ NumberGuessing/src/guessinggame/GuessingGame.java
@@ -3,31 +3,31 @@ import javax.swing.*;
 
 public class GuessingGame  {
     public static void main(String[] args) {
-        int computerNumber = (int) (Math.random()*100 + 1);
+        int ComputerNumber = (int) (Math.random()*100 + 1);
         int userAnswer = 0;
-        System.out.println("The correct guess would be " + computerNumber);
+        System.out.println("The correct guess would be " + ComputerNumber);
         int count = 1;
 
-        while (userAnswer != computerNumber)
+        while (userAnswer != ComputerNumber)
         {
             String response = JOptionPane.showInputDialog(null,
                 "Enter a guess between 1 and 100", "Guessing Game", 3);
             userAnswer = Integer.parseInt(response);
-            JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, computerNumber, count));
+            JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, ComputerNumber, count));
             count++;
         }  
     }
-    public static String determineGuess(int userReponse, int computerNumber, int count){
+    public static String determineGuess(int userReponse, int ComputerNumber, int count){
         if (userReponse <=0 || userReponse >100) {
             return "Your guess is invalid";
         }
-        else if (userReponse == computerNumber ){
+        else if (userReponse == ComputerNumber ){
             return "Correct!\nTotal Guesses: " + count;
         }
-        else if (userReponse > computerNumber) {
+        else if (userReponse > ComputerNumber) {
             return "Your guess is too high, try again.\nTry Number: " + count;
         }
-        else if (userReponse < computerNumber) {
+        else if (userReponse < ComputerNumber) {
             return "Your guess is too low, try again.\nTry Number: " + count;
         }
         else {
